64973bcb9aa1147534bc6aa6cf054ab722892314
MoSangBin <mosangbin94@gmail.com>
2022-11-02 03:20:26 +0900

    Chore: 공통 fontSize 설정

--- src/assets/theme.ts
+++ src/assets/theme.ts
@@ -7,4 +7,10 @@ export const theme: DefaultTheme = {
     gray: "#D9D9D9",
     blue: "#0094FF",
   },
+  fontSize: {
+    small: "12px",
+    medium: "14px",
+    large: "17px",
+    xLarge: "21px",
+  },
 };
--- src/types/styled-components.d.ts
+++ src/types/styled-components.d.ts
@@ -3,10 +3,16 @@ import "styled-components";
 declare module "styled-components" {
   export interface DefaultTheme {
     colors: {
-      white: "#ffffff";
-      black: "#000000";
-      gray: "#D9D9D9";
-      blue: "#0094FF";
+      white: string;
+      black: string;
+      gray: string;
+      blue: string;
+    };
+    fontSize: {
+      small: string;
+      medium: string;
+      large: string;
+      xLarge: string;
     };
   }
 }
