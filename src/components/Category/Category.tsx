104d0a9afb8b71f8625d1e2e1bd0564cba64961c
MoSangBin <mosangbin94@gmail.com>
2022-11-02 04:38:23 +0900

    Add: Category 컴포넌트 추가

---
+++ src/components/Category/Category.tsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+import { TCategory } from "../../types";
+import { CategoryButton } from "../Buttons";
+
+export const Container = styled.menu`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  padding: 0 12px;
+  height: 40px;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.black};
+`;
+
+const CATEGORY_LIST: Array<TCategory> = ["전체", "대형", "중형", "소형"];
+
+interface Props {
+  sortOption: string;
+  changeSortOption: React.Dispatch<React.SetStateAction<TCategory>>;
+}
+
+const Category = ({ sortOption, changeSortOption }: Props) => {
+  return (
+    <Container>
+      {CATEGORY_LIST.map((category) => (
+        <CategoryButton
+          key={category}
+          title={category}
+          isClicked={category === sortOption}
+          onClick={() => changeSortOption(category)}
+        />
+      ))}
+    </Container>
+  );
+};
+
+export default Category;
