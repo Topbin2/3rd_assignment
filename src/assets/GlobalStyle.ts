66e950ff5a4b46655b78f8eb18649fa8f26b4e6b
MoSangBin <mosangbin94@gmail.com>
2022-11-02 03:33:21 +0900

    Inter 폰트 적용

--- src/assets/GlobalStyle.ts
+++ src/assets/GlobalStyle.ts
@@ -3,15 +3,16 @@ import { reset } from "styled-reset";
 
 export const GlobalStyle = createGlobalStyle`
   ${reset}
-
+  
   * {
     box-sizing: border-box;
   }
-
+  
   html,
   body {
     width: 100%;
     height: 100%;
+    font-family: 'Inter', sans-serif;
   }
 
   ol, ul, li {
