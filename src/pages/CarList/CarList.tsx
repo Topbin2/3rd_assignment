cc372fec8d6747f9c586d2ea726a6c15ea893c62
MoSangBin <mosangbin94@gmail.com>
2022-11-03 01:13:13 +0900

    Feat: CarList 페이지, 세그먼트에 맞게 sort 기능 추가

--- src/pages/CarList/CarList.tsx
+++ src/pages/CarList/CarList.tsx
@@ -1,5 +1,57 @@
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import styled from "styled-components";
+
+import { getCarList } from "../../apis";
+import { CarCard, Category, Header } from "../../components";
+import { TCategory } from "../../types";
+import { convertCategoryToSegment } from "../../utils";
+
+export const MessageContainer = styled.div`
+  display: felx;
+  justify-content: center;
+  align-items: center;
+  min-height: calc(100vh - 100px);
+  font-size: ${({ theme }) => theme.fontSize.large};
+  font-weight: 700;
+`;
+
 const CarList = () => {
-  return <div>CarList</div>;
+  const [sortOption, setSortOption] = useState<TCategory>("전체");
+  const { data, isLoading } = useQuery({
+    queryKey: ["cars", sortOption],
+    queryFn: () =>
+      getCarList({ segment: convertCategoryToSegment(sortOption) }),
+  });
+
+  return (
+    <>
+      <Header title="전체차량" isPrevButton={false} />
+      <Category sortOption={sortOption} changeSortOption={setSortOption} />
+      <ul>
+        {isLoading && <MessageContainer>불러오는 중</MessageContainer>}
+        {data && !data.length && (
+          <MessageContainer>차량이 없습니다.</MessageContainer>
+        )}
+        {data?.map((car) => {
+          const { id, attribute, amount, createdAt } = car;
+          const { name, brand, segment, fuelType, imageUrl } = attribute;
+          return (
+            <CarCard
+              key={id}
+              name={name}
+              brand={brand}
+              segment={segment}
+              fuelType={fuelType}
+              imgUrl={imageUrl}
+              amount={amount}
+              createdAt={createdAt}
+            />
+          );
+        })}
+      </ul>
+    </>
+  );
 };
 
 export default CarList;
